c7819ad966b954ed2ac48b3a9001507c38e2e377
Bill <billcc.lv@hotmail.com>
2017-02-06 10:15:38 +0800

    Android BLE samples

---
+++ liteble/src/main/java/com/yinuo/liteble/conn/BleRssiCallback.java
@@ -0,0 +1,11 @@
+package com.yinuo.liteble.conn;
+
+/**
+ * callback of RSSI read.
+ *
+ * @author MaTianyu @http://litesuits.com
+ * @date 2015-11-21
+ */
+public abstract class BleRssiCallback extends BleCallback {
+    public abstract void onSuccess(int rssi);
+}
